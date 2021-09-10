83109e25a9cc3c7fdf7005ceb11bd8085a5a6c5d
Vu Le <vu.le@zalora.com>
2021-09-10 16:17:40 +0700

    GSP: run notebook from bash

---
+++ py/run_notebook.py
@@ -0,0 +1,9 @@
+import fire, nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+def run_notebook(path):
+    nb = nbformat.read(open(path), as_version=nbformat.NO_CONVERT)
+    ExecutePreprocessor(timeout=600).preprocess(nb, {})
+    print('Done')
+
+if __name__ == '__main__': fire.Fire(run_notebook)
